2cc0cc1e68a041c31c99f6d9a5f5fc2d179a13d6
EthanQ-EQ <zhih3561@gmail.com>
2026-07-23 17:14:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,17 @@
 alert("script.js 已加载");
+
 const hall = document.getElementById("hall-hitbox");
 const popup = document.getElementById("hall-popup");
-
 const closeBtn = document.getElementById("closeBtn");
-const upgradeBtn = document.getElementById("upgradeBtn");
 
-const hallLevel = document.getElementById("hallLevel");
-const wood = document.getElementById("wood");
-const food = document.getElementById("food");
+console.log(hall);
+console.log(popup);
 
-let level = 1;
-let woodNum = 5000;
-let foodNum = 6000;
-
-hall.addEventListener("click", () => {
+hall.addEventListener("click", function () {
+    alert("点击到了城主府");
     popup.style.display = "block";
 });
 
-closeBtn.addEventListener("click", () => {
+closeBtn.addEventListener("click", function () {
     popup.style.display = "none";
-});
-
-upgradeBtn.addEventListener("click", () => {
-
-    if (woodNum < 500 || foodNum < 500) {
-        alert("资源不足");
-        return;
-    }
-
-    woodNum -= 500;
-    foodNum -= 500;
-    level++;
-
-    hallLevel.textContent = level;
-    wood.textContent = woodNum;
-    food.textContent = foodNum;
-
 });
